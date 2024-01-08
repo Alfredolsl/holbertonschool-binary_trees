aaf428c4b6566660337a23fd5007b6705e604c3f
Alfredolsl <alfredo.445566@gmail.com>
2024-01-08 10:53:51 -0800

    betty is chill now

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,18 +12,19 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
-	if (parent == NULL)
-		return (NULL);
-
 	new = malloc(sizeof(binary_tree_t));
-
 	if (new == NULL)
 		return (NULL);
 
 	new->n = value;
-	new->parent = parent;
+
+	if (value < parent->n)
+		parent->left = new;
+	else if (parent->n >= value)
+		parent->right = new;
+
 	new->left = NULL;
 	new->right = NULL;
 
-	return (new);	
+	return (new);
 }
