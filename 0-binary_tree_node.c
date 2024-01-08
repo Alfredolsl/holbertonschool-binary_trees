0bd1a2126e1d2243f8739a7c5de66d9b3acbfe53
Alfredolsl <alfredo.445566@gmail.com>
2024-01-08 10:41:47 -0800

    New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -15,21 +15,15 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (parent == NULL)
 		return (NULL);
 
+	new = malloc(sizeof(binary_tree_t));
+
+	if (new == NULL)
+		return (NULL);
+
 	new->n = value;
+	new->parent = parent;
 	new->left = NULL;
 	new->right = NULL;
-
-	
-	if (value < parent->n)
-	{
-		if (parent->left == NULL)
-			parent->left = new;
-	}
-	else if (value > parent->n)
-	{
-		if (parent->right == NULL)
-			parent->right = new;
-	}
 
 	return (new);	
 }
